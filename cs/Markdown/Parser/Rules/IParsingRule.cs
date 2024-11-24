350c559a2bbddab6f9f633fb08cf86b517a7649a
vafle228 <dimasmyslaev228@gmail.com>
2024-11-24 19:38:50 +0500

    Добавил парсеры

---
+++ cs/Markdown/Parser/Rules/IParsingRule.cs
@@ -0,0 +1,9 @@
+﻿using Markdown.Parser.Nodes;
+using Markdown.Tokenizer.Tokens;
+
+namespace Markdown.Parser.Rules;
+
+public interface IParsingRule
+{
+    public Node Match(List<Token> tokens);
+}
