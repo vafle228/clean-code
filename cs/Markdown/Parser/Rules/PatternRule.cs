676fd396123a6ecbcdbbb3beefd1fd36a6d30d85
vafle228 <dimasmyslaev228@gmail.com>
2024-12-01 14:03:05 +0500

    Паттерн рул

---
+++ cs/Markdown/Parser/Rules/PatternRule.cs
@@ -0,0 +1,23 @@
+﻿using Markdown.Parser.MatchTools.Models;
+using Markdown.Parser.Nodes;
+using Markdown.Tokenizer.Tokens;
+
+namespace Markdown.Parser.Rules;
+
+public class PatternRule(List<TokenType> pattern) : IParsingRule
+{
+    public Node? Match(List<Token> tokens, int begin = 0)
+    {
+        if (pattern.Count == 0) return null;
+        if (tokens.Count - begin < pattern.Count) return null;
+        
+        var isMatched = tokens
+            .Skip(begin).Take(pattern.Count).Zip(pattern)
+            .All(pair => pair.First.TokenType == pair.Second);
+        
+        if (!isMatched) return null;
+        var match = new TokenMatch(begin, pattern.Count, tokens);
+
+        return new Node(NodeType.TEXT, match, pattern.Count);
+    }
+}
---
+++ cs/MarkdownTests/Parser/MatchTools/PatternRuleTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Markdown.Parser.Rules;
+using Markdown.Tokenizer;
+using Markdown.Tokenizer.Tokens;
+
+namespace MarkdownTests.Parser.MatchTools;
+
+[TestFixture]
+public class PatternRuleTest
+{
+    private readonly MarkdownTokenizer tokenizer = new();
+    
+    [Test]
+    public void PatternRule_Match_SinglePattern()
+    {
+        var tokens = tokenizer.Tokenize("_");
+        var rule = new PatternRule([TokenType.UNDERSCORE]);
+        
+        var node = rule.Match(tokens);
+        
+        node.Should().NotBeNull();
+        node.Value.Should().NotBeNull();
+        node.Value.ToList().Should().BeEquivalentTo(tokens);
+    }
+
+    [Test]
+    public void PatternRule_Match_ContinuesPattern()
+    {
+        var tokens = tokenizer.Tokenize("_\n ");
+        var rule = new PatternRule([TokenType.UNDERSCORE, TokenType.NEW_LINE, TokenType.SPACE]);
+        
+        var node = rule.Match(tokens);
+        
+        node.Should().NotBeNull();
+        node.Value.Should().NotBeNull();
+        node.Value.ToList().Should().BeEquivalentTo(tokens);
+    }
+}
